8b70cc5a6649262a158fe216a6096350b63610f5
konda <32452084+Kondiitsio@users.noreply.github.com>
2022-12-06 19:56:27 +0200

    Custom post type Finished

--- build/blocks/recipe-summary/frontend.asset.php
+++ build/blocks/recipe-summary/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element'), 'version' => '851fab0a454a7eab4cc3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element'), 'version' => 'c1f411329a99ead834e7');
